62034b04b5c1819cf64820a4917e386b43e5f927
shpeng <shpeng10332@gmail.com>
2021-08-06 00:05:11 +0800

    upload health center api

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# HappyClickService 
-## Health center API
-
-安裝方式： pip install -r requirement.txt
+# HappyClickService
+目前架設在 heroku server 上，可以直接用postman之類的測試
+## 使用方式
+- 查詢預約接種名單 url : https://happyclick-healthcenter.herokuapp.com/SearchFormData
+- 上傳接種資料 url : https://happyclick-healthcenter.herokuapp.com/UpdateVaccinated
+# Health center API
+### SearchFormData 查詢預約接種名單
+- Input : 
+    - date (string, ex. {"date":"2021/08/12"})
+- Output : 
+    - 有資料 
+        - list[{form_id(integer), vaccine_type(string), ID(integer), Name(string)}, .....]
+    - 沒資料 
+        - {'msg' : 'No FormData data!'}
+### UpdateVaccinated 上傳接種資料
+- Input : 
+    - {form_id(integer), ID(integer), Name(string)}
+- Output : 
+    - {'msg' : 'Update Vaccinated successful!'}
--- app.py
+++ app.py
@@ -14,31 +14,35 @@ app = Flask(__name__)
 api = Api(app)
 conn = db.connection()
 
-class Test(Resource):
-    def get(self):
-        print('hello world')
-        return jsonify({'msg':'Hello World'})
-
 class UpdateVaccinated(Resource):
     def post(self):
         # get data from frontend json
-        data = request.get_json(force = True)
-        conn.happyclick.VaccinatedData.insert_one({"vaccinated_id":data["id"], 
-                                                   "vaccinated_name":data["name"], 
-                                                   "vaccine_type":data["type"],
-                                                   "vaccine_date":data["date"]})
-        vdata = conn.happyclick.VaccinatedData.find({"vaccinated_id":120452})
-        for data in vdata:
-            print(data)
-        return jsonify({'msg':'Check reserve successful!'})
+        vaccinated_data = request.get_json(force = True)
+        conn.happyclick.FormData.update_one({"form_id" : vaccinated_data["form_id"]},{"$set" : {"status" : True}})
+        db_vaccinated_data = conn.happyclick.VaccinatedData.find_one({"ID" : vaccinated_data["ID"]})
+        if db_vaccinated_data is None:
+            conn.happyclick.VaccinatedData.insert({"ID" : vaccinated_data["ID"] ,"Name" : vaccinated_data["Name"], "vaccinated_times" : 0})
+        conn.happyclick.VaccinatedData.update({"ID" : vaccinated_data["ID"]}, {"$inc" : {"vaccinated_times" : 1}})
+        return jsonify({'msg' : 'Update Vaccinated successful!'})
 
-    def get(self):
-        data = conn.happyclick.VaccinatedData.find()
-        return jsonify({})
+class SearchFormData(Resource):
+    def post(self):
+        datas_to_front = []
+        date_data = request.get_json(force = True)
+        datas_from_db = conn.happyclick.FormData.find({"date" : date_data["date"], "status" : False})
+        for data in datas_from_db:
+            formData_dict = {"form_id" : data["form_id"], 
+                            'vaccine_type' : data['vaccine_type'],
+                            'ID' : data['ID'],
+                            "Name" : str(data["Name"])}
+            datas_to_front.append(formData_dict)
 
+        if len(datas_to_front) == 0:
+            return jsonify({'msg' : 'No FormData data!'})
+        return jsonify(datas_to_front)
 
 api.add_resource(UpdateVaccinated, '/UpdateVaccinated')
-api.add_resource(Test, '/')
+api.add_resource(SearchFormData, '/SearchFormData')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8088, threaded=True, debug=True)
