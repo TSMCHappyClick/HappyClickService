05ee324f17308ab7303750a0c5ed11edd56566a1
ben980828 <ben980828@gmail.com>
2021-08-07 16:32:56 +0800

    fix ID String issue

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from dns.rdatatype import NULL
-from flask import Flask, flash, redirect, url_for, session, request, logging
+from flask import Flask, request, render_template
 from flask.globals import request
 from flask.json import jsonify
 from flask_restful import Api, Resource
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_pymongo import PyMongo
 import pymongo
 from sqlalchemy import func
 import warnings
 import json
+import os
 from datetime import datetime
 from sqlalchemy.sql.elements import Null
 
@@ -20,42 +22,113 @@ warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 api = Api(app)
-# 1. Maria DB
-# username = 'root'           
-# password = 'adkasghdih1919' # self pass    
-# host = '127.0.0.1'          
-# port = '3306'               
-# database = 'hackathon'    # self db
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db.init_app(app)
-
-# 2. Mongo DB
+# Mongo DB
 conn = db.connection()
-
 
 class Home(Resource):
     def get(self):
         print('hello world')
         return jsonify({'msg':'Hello World'})
+conn = db.connection()
 
+# Set up session's secret key (for 加密) and load database config
+app.config['SECRET_KEY'] = os.urandom(24)
+
+# Bundle flask and flask-login together
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'login'
+
+
+class User(UserMixin):
+    pass
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
+
+
+def query_user(user_id):
+    return list(conn.happyclick.UserData.find({"ID":user_id}))
+    
+def find_employees_under_staff(staff_id):
+    staffs = list(conn.happyclick.StaffData.find({'ID':staff_id}))
+    for employeeID in staffs[0]['employees']:
+        # find staff 底下 employee 注射狀況
+        employees = list(conn.happyclick.FormData.find({'ID':employeeID}))
+        for employee in employees:
+            if employee['status']:
+                print(employee)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    if query_user(user_id) is not None:
+        curr_user = User()
+        curr_user.id = user_id
+
+        return curr_user
+
+
+class Login(Resource):
+    def get():
+        return render_template('login.html')
+    def post(self):
+        data = request.get_json(force = True)
+
+        ID = int(data['ID'])
+        password = data['password']
+        print('ID:{},password:{}'.format(ID,password))
+        
+        user = query_user(ID)
+
+        if user is not None and password == user[0]['password']:
+
+            curr_user = User()
+            curr_user.id = ID
+
+            # 通過Flask-Login的login_user方法登入使用者Í
+            login_user(curr_user)
+            print('Succesfully login!')
+            # 回傳true給前端去做redirect page
+            if ID in db.meds:
+                return jsonify ({
+                    'identity':'med'
+                })
+            return jsonify({
+                'identity' : 'employee'
+            })
+            
+        print('Login fail!')
+        return  jsonify({'identity':'Wrong id or password!'})
+
+
+
+@login_required
+@app.route('/logout')  
+def logout():
+    logout_user()
+    return jsonify({'msg':'Logged out successfully!'})
 
 def checkVaccineAmount(vaccDate, vaccType):
     # Compare reserve and vaccine total amount to prevend out of vaccine
     vaccineRecord = conn.happyclick.VaccineData.find_one({'date': vaccDate, 'vaccine_type': vaccType})
     reserveAmount = vaccineRecord['reserve_amount']
     vaccineAmount = vaccineRecord['vaccine_amount']
-
-    # print("\nCurrent vaccDate in check:{}".format(str(vaccDate)))
-    # print("\nCurrent reserveAmount in check:{}".format(str(reserveAmount)))
-    # print("\nCurrent vaccineAmount in check:{}".format(str(vaccineAmount)))
-
+    
     if reserveAmount < vaccineAmount:
         return True # return true if reserve less than total amount
     else : 
         return False # return false if reserve equals to total amount (or other exception)
 
+@login_required
 class SaveReserve(Resource):
     def post(self):
         # get data from frontend json
@@ -74,7 +147,7 @@ class SaveReserve(Resource):
 
         if (checkVaccineAmount(vaccDate, vaccType)):
             # add data to DB
-            conn.happyclick.FormData.insert_one({'form_id': formId, 'ID': userId, 'Name': username, 'vaccine_type': vaccType, 'date': vaccDate, 'status': False})
+            conn.happyclick.FormData.insert_one({'form_id': formId, 'ID': int(userId), 'Name': username, 'vaccine_type': vaccType, 'date': vaccDate, 'status': False})
             # update reserve amount
             vaccineRecord = conn.happyclick.VaccineData.find_one({'date': vaccDate, 'vaccine_type': vaccType})
             conn.happyclick.VaccineData.update_one({'date': vaccDate, 'vaccine_type': vaccType}, 
@@ -85,11 +158,12 @@ class SaveReserve(Resource):
         else :
             return jsonify({'msg':'Reservation chosen is full!'})
 
+@login_required
 class CheckReserve(Resource):
     def post(self):
         data = request.get_json(force = True)
         userId = data["ID"]
-        reserveRecord = conn.happyclick.FormData.find_one({'ID': userId, 'status': False}) # prevend DB from query vaccinated record
+        reserveRecord = conn.happyclick.FormData.find_one({'ID': int(userId), 'status': False}) # prevend DB from query vaccinated record
         if reserveRecord :
             print('\nFind one unvaccinated reserve!')
             vaccine_type = reserveRecord['vaccine_type']
@@ -100,6 +174,7 @@ class CheckReserve(Resource):
         else:
             return jsonify({'msg': 'No reservation found!'})
 
+@login_required
 class RemoveReserve(Resource):
     def post(self):
         # get data from frontend json
@@ -108,7 +183,7 @@ class RemoveReserve(Resource):
         vaccDate = data["date"]
         vaccType = data["vaccine_type"]
         # send request to delete data in DB
-        reserveRecord = conn.happyclick.FormData.find_one({'ID': userId, 'date': vaccDate, 'vaccine_type': vaccType, 'status': False})
+        reserveRecord = conn.happyclick.FormData.find_one({'ID': int(userId), 'date': vaccDate, 'vaccine_type': vaccType, 'status': False})
         if reserveRecord:
             print('\nFind one unvaccinated reserve!')
             conn.happyclick.FormData.delete_one({'ID': userId, 'date': vaccDate, 'vaccine_type': vaccType})
@@ -138,7 +213,7 @@ class ReturnAvailable(Resource):
 
 
 api.add_resource(Home, '/')
-# api.add_resource(Login, "/Login")
+api.add_resource(Login, "/Login")
 api.add_resource(SaveReserve,  '/Reserve')
 api.add_resource(CheckReserve,  '/Check')
 api.add_resource(RemoveReserve,  '/Remove')
