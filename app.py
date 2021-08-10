637885e28322ac8a072dbe38f84deb22f47f082a
ntudannylee <43718977+ntudannylee@users.noreply.github.com>
2021-08-10 21:53:05 +0800

    cors fixed

--- app.py
+++ app.py
@@ -40,9 +40,13 @@ class User(UserMixin):
 
 @app.after_request
 def after_request(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+    response.headers['Access-Control-Allow-Origin'] = 'https://happy-click.herokuapp.com'
+    response.headers.add('Access-Control-Allow-Headers',
+                        'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin, Accept, '
+                        'X-Requested-With, Content-Type, '
+                        'Access-Control-Request-Method, Access-Control-Request-Headers')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
     
     return response
 
